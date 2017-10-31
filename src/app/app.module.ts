3dc1a43e85b128fa6a6fd83e6467a047b02a909d
Maikel <maikelpatrick@gmail.com>
2017-10-31 20:55:32 -0200

    Estudos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,27 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import { ROUTES} from './app.routes'
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { HeardComponent } from './heard/heard.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeardComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantesComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heard/heard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-heard',
+  templateUrl: './heard.component.html'
+})
+export class HeardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
